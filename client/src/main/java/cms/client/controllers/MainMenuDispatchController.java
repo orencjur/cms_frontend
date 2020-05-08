5ec25411e181d2ca1dfdbb1f60f29172e7fd4cb5
pejchpet <pejchpet@fel.cvut.cz>
2020-05-08 11:04:52 +0200

    name in dispatch menu

--- client/src/main/java/cms/client/controllers/MainMenuDispatchController.java
+++ client/src/main/java/cms/client/controllers/MainMenuDispatchController.java
@@ -3,10 +3,17 @@ package cms.client.controllers;
 import cms.client.view.FxmlView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class MainMenuDispatchController extends AbstractController {
 
 
+    public Label name;
+
+    @FXML
+public void initialize() {
+    name.setText(stageManager.getSession().getLoggedUser());
+}
 
     @FXML
     public void shipments(ActionEvent event){
